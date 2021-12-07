a14594ff1655747d3a438e887fc75234368e6d9d
Daniyel Zhumankulov <d.zhumankulov@alabs.team>
2021-12-07 23:35:42 +0600

    added everything

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import Head from 'next/head';
+import {useState} from "react";
+import {UserContext} from "../components/UserContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const [user, setUser] = useState(null);
+  return (
+      <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet"/>
+      </Head>
+          <UserContext.Provider value={[user, setUser]}>
+              <Component {...pageProps} />
+          </UserContext.Provider>
+      </>
+  );
+
 }
 
 export default MyApp
